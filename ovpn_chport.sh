b99258909c60f476e8e45a5535c2c14446252f54
sunchao62 <sunchao11@jd.com>
2021-11-25 15:01:08 +0800

    save

---
+++ ovpn_chport.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+SERVER_CONF=/etc/openvpn/server.conf
+ADD_RULE=/etc/iptables/add-openvpn-rules.sh
+RM_RULE=/etc/iptables/rm-openvpn-rules.sh
+# SERVER_CONF=server.conf
+# ADD_RULE=add-openvpn-rules.sh
+# RM_RULE=rm-openvpn-rules.sh
+
+if [ $# -lt 1 ]; then
+    echo "Wrong usage!"
+    exit 0
+fi
+
+if [[ $1 -lt 1025 || $1 -gt 65535 ]]; then
+    echo "$1 is not a valid port number!"
+    exit 0
+fi
+
+new_port=$1
+old_port=`grep 'port' $SERVER_CONF | awk '{print $NF}'`
+if [ -z "$old_port" ]; then
+    echo "Cannot detect old port!"
+    exit 0
+fi
+
+echo "Changing ovpn port from $old_port to $new_port"
+# set server.conf
+sed -i -e "s/port\ *$old_port/port $new_port/g" $SERVER_CONF
+# rules
+sed -i -e "s/--dport\ *$old_port/--dport $new_port/g" $ADD_RULE
+sed -i -e "s/--dport\ *$old_port/--dport $new_port/g" $RM_RULE
+
+reboot
+
+
+
+
